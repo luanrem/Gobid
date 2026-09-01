5f56fdd9046234866d22459ac51f478719e3dbc6
luanrem <luanrem@gmail.com>
2026-09-01 15:00:39 +0200

    01 - estrutura da api e rotas

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/luanrem/Gobid
 
 go 1.25.1
 
-require github.com/joho/godotenv v1.5.1
+require (
+	github.com/go-chi/chi/v5 v5.3.2
+	github.com/joho/godotenv v1.5.1
+)
---
+++ internal/api/api.go
@@ -0,0 +1,15 @@
+package api
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+type Api struct {
+	Router *chi.Mux
+}
+
+func (api *Api) handleCreateUser(w http.ResponseWriter, r *http.Request) {
+	// Handle user creation logic here
+}
