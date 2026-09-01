2a578ab92e3f9813be4c07118c60b147e0c0bb4c
luanrem <luanrem@gmail.com>
2026-09-01 16:07:49 +0200

    02-finalizando-o-input-de-usuario-com-jsonutils

--- internal/api/user_handlers.go
+++ internal/api/user_handlers.go
@@ -1,9 +1,19 @@
 package api
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/luanrem/Gobid/internal/jsonutils"
+	"github.com/luanrem/Gobid/internal/usecase/user"
+)
 
 func (api *Api) handleSignupUser(w http.ResponseWriter, r *http.Request) {
-	panic("TODO - NOT IMPLEMENTED")
+	data, problems, err := jsonutils.DecodeValidJson[user.CreateUserReq](r)
+	if err != nil {
+		_ = jsonutils.EncodeJson(w, r, http.StatusUnprocessableEntity, problems)
+	}
+
+	// create user
 }
 
 func (api *Api) handleLoginUser(w http.ResponseWriter, r *http.Request) {
--- internal/usecase/user/create_user.go
+++ internal/usecase/user/create_user.go
@@ -7,10 +7,10 @@ import (
 )
 
 type CreateUserReq struct {
-	UserName     string `json:"user_name"`
-	Email        string `json:"email"`
-	PasswordHash []byte `json:"password_hash"`
-	Bio          string `json:"bio"`
+	UserName string `json:"user_name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+	Bio      string `json:"bio"`
 }
 
 func (req CreateUserReq) Valid(ctx context.Context) validator.Evaluator {
@@ -18,7 +18,10 @@ func (req CreateUserReq) Valid(ctx context.Context) validator.Evaluator {
 
 	eval.CheckField(validator.NotBlank(req.UserName), "user_name", "this field cannot be empty")
 	eval.CheckField(validator.NotBlank(req.Email), "email", "this field cannot be empty")
+	eval.CheckField(validator.Matches(req.Email, validator.EmailRX), "email", "this field must be a valid email address")
 	eval.CheckField(validator.NotBlank(req.Bio), "bio", "this field cannot be empty")
+	eval.CheckField(validator.MinChars(req.Bio, 10) && validator.MaxChars(req.Bio, 200), "bio", "this field must have a lenght between 10 and 255	characters")
+	eval.CheckField(validator.MinChars(req.Password, 8), "password", "password must be bigger than 8 characters")
 
 	return eval
 }
